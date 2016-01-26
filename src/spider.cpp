1fe3b39c7e9e68cb0cde063d7d684c40386a2656
tangyumin <tangyumin@letv.com>
2016-01-26 12:08:52 +0800

    spider init

---
+++ src/spider.cpp
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void daemon(){
+	pid_t pid;
+	if((pid =fork())<0){
+		perror("fork");
+		exit(1);
+	}else if(pid>0){
+		exit(1);
+	}
+
+	setsid();
+}
+
+static void version(){
+	printf("version 1.0\n");
+	exit(1);
+}
+
+static void usage(){
+	printf("spider Usage [OPTION]\n"
+	" -h\t help\n"
+	" -v\t version\n"
+	" -d\t run as deamon\n");
+	exit(1);
+}
+
+int main(int argc,char **argv){
+	char ch;
+	int daemoned=0;
+	while((ch =getopt(argc,argv,"vhd"))!=-1){
+		switch(ch){
+			case 'v':
+				version();
+				break;
+			case 'd':
+				daemoned=1;
+				break;
+			case 'h':
+			case '?':
+			default :
+				usage();
+				break;
+		}
+	}
+	
+	if(daemoned==1){
+		daemon();
+	}
+
+	while(1){};
+
+	return 0;
+}
+
